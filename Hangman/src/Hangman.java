eb6ca7302f7db4597a412d9f11a5baca32d9eadf
Zachary Vandeventer <zachv94@gmail.com>
2021-08-20 11:13:17 -0400

    broke the hangman game into parts, minimalized main usage

--- Hangman/src/Hangman.java
+++ Hangman/src/Hangman.java
@@ -3,81 +3,150 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Hangman {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
 
-        ArrayList<String> words = new ArrayList<>() {{
-            add("cat");
-            add("dog");
-            add("apple");
-            add("car");
-            add("coffee");
+    private static final ArrayList<String> words = new ArrayList<>() {{
+        add("cat");
+        add("dog");
+        add("apple");
+        add("car");
+        add("coffee");
+    }};
+    private static final String[] bodyParts = new String[]{"O", "|", "|", "/", "\\"};
+    private static ArrayList<String> missedLetters;
+    private static String missed;
+    private static ArrayList<String> guessWord;
+    private static ArrayList<String> guessedList;
+    private static String guessed;
+    private static ArrayList<String> body;
+    private static int misses;
+
+    private static void setup() {
+        missedLetters = new ArrayList<>();
+        missed = "";
+        guessWord = getGuessedWordLetters();
+        guessedList = new ArrayList<>();
+        guessed = createEmptyGuessedString();
+        misses = 0;
+        body = initializeBodyArray();
+
+    }
+
+    private static ArrayList<String> getGuessedWordLetters() {
+        return new ArrayList<>() {{
+            this.addAll(Arrays.asList(words.get((int) Math.floor(Math.random() * words.size())).split("")));
         }};
+    }
+
+    private static String createEmptyGuessedString() {
+        for (String x : guessWord) {
+            guessedList.add("_");
+        }
+        return stringifyList(guessedList);
+    }
+
+    private static String stringifyList(ArrayList l) {
+        StringBuilder s =new StringBuilder();
+        for (var item : l) {
+            s.append(item);
+        }
+        return s.toString();
+    };
+
+    private static ArrayList<String> initializeBodyArray() {
+        return new ArrayList<>() {{
+            for (int i = 0; i < 5; i++) {
+                add(" ");
+            }
+        }};
+    }
+
+    private static void addBodyPartToGallow(){
+        for (int i = 0; i < misses; i++) {
+            body.set(i, bodyParts[i]);
+        }
+    }
+
+    private static void printGallow() {
+        System.out.println("\n+---+");
+        System.out.printf("%s   |\n", body.get(0));
+        System.out.printf("%s   |\n", body.get(1));
+        System.out.printf("%s   |\n", body.get(2));
+        System.out.printf("%s%s===\n", body.get(3), body.get(4));
+
+        System.out.printf("missed: %s\n ", missed);
+
+        System.out.printf("%s\n", guessed);
+    }
+
+    public static boolean wordFound() {
+        boolean hasMissingLetter = false;
+        for (String l : guessedList) {
+            if (l.equals("_")) {
+                hasMissingLetter = true;
+            }
+        }
+        return !hasMissingLetter;
+    }
+
+    public static boolean playAgainPrompt() {
+        String yesNo = sc.nextLine();
+        return yesNo.equals("no");
+    }
+
+    public static void checkInput(String input) {
+        if (input.length() != 1) {
+            System.out.println("Invalid input Try again");
+        } else {
+            boolean inMissedLetters = false;
+            for (var l : missedLetters) {
+                if (input.equals(l)) {
+                    inMissedLetters = true;
+                }
+            }
+            if (inMissedLetters) {
+                System.out.println("You've already chosen that letter.");
+            } else {
+                boolean match = false;
+                for (int i = 0; i < guessWord.size(); i++) {
+                    if (input.equals(guessWord.get(i))) {
+                        match = true;
+                        guessedList.set(i, guessWord.get(i));
+                        guessed = stringifyList(guessedList);
+                    }
+                }
+                if (!match) {
+                    misses += 1;
+                    missedLetters.add(input);
+                    missed = stringifyList(missedLetters);
+                    addBodyPartToGallow();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
         boolean exit = false;
         while (!exit){
-            ArrayList<String> missedLetters = new ArrayList<>();
-            StringBuilder missedString = new StringBuilder();
-            for (var l : missedLetters) {
-                missedString.append(l);
-            }
-
-            ArrayList<String> guessWord = new ArrayList<>() {{
-                this.addAll(Arrays.asList(words.get((int) Math.floor(Math.random() * words.size())).split("")));
-            }};
-            ArrayList<String> guessed = new ArrayList<>();
-            for (String l : guessWord) {
-                guessed.add("_");
-            }
-            StringBuilder guessedString = new StringBuilder();
-            for (var l : guessed) {
-                guessedString.append(l);
-            }
-            int misses = 0;
-            ArrayList<String> body = new ArrayList<>() {{
-                for (int i = 0; i < 5; i++) {
-                    add(" ");
-                }
-            }};
-            String[] bodyParts = {"O", "|", "|", "/", "\\"};
+            setup();
 
             System.out.println("\nH A N G M A N");
 
             boolean gameRunning = true;
             while (gameRunning) {
 
-                System.out.println("\n+---+");
-                System.out.printf("%s   |\n", body.get(0));
-                System.out.printf("%s   |\n", body.get(1));
-                System.out.printf("%s   |\n", body.get(2));
-                System.out.printf("%s%s===\n", body.get(3), body.get(4));
-
-                System.out.printf("missed: %s\n ", missedString);
-
-                System.out.printf("%s\n", guessedString);
+                printGallow();
 
                 if (body.get(4).equals(bodyParts[4])) {
                     gameRunning = false;
                     System.out.println("Game Over! Play again?");
-                    String yesNo = sc.nextLine();
-                    if (yesNo.equals("no")) {
-                        exit = true;
-                    }
+                    exit = playAgainPrompt();
                     continue;
                 }
-                boolean notGuessed = true;
-                for (String l : guessed) {
-                    if (l.equals("_")){
-                        notGuessed = false;
-                        break;
-                    }
-                }
-                if (notGuessed) {
+                if (wordFound()) {
                     gameRunning = false;
                     System.out.println("You did it! Play again?");
-                    String yesNo = sc.nextLine();
-                    if (yesNo.equals("no")) {
-                        exit = true;
-                    }
+                    exit = playAgainPrompt();
                     continue;
                 }
 
@@ -85,43 +154,7 @@ public class Hangman {
 
                 String input = sc.nextLine();
 
-                if (input.length() != 1) {
-                    System.out.println("Invalid input Try again");
-                }
-
-                boolean inMissedLetters = false;
-                for (var l : missedLetters) {
-                    if (input.equals(l)) {
-                        inMissedLetters = true;
-                    }
-                }
-                if (inMissedLetters) {
-                    System.out.println("You've already chosen that letter.");
-                }
-                else {
-                    boolean match = false;
-                    for (int i = 0; i < guessWord.size(); i++) {
-                        if (input.equals(guessWord.get(i))) {
-                            match = true;
-                            guessed.set(i, guessWord.get(i));
-                            guessedString = new StringBuilder("");
-                            for (var l : guessed) {
-                                guessedString.append(l);
-                            }
-                        }
-                    }
-                    if (!match) {
-                        misses += 1;
-                        missedLetters.add(input);
-                        missedString = new StringBuilder();
-                        for (var l : missedLetters) {
-                            missedString.append(l);
-                        }
-                        for (int i = 0; i < misses; i++) {
-                            body.set(i, bodyParts[i]);
-                        }
-                    }
-                }
+                checkInput(input);
             }
         }
     }
